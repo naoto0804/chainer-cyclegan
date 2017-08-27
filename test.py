9f562dd7d06086a83ee3bb2da3a61fabffffa39e
Naoto Inoue <inoue@hal.t.u-tokyo.ac.jp>
2017-08-27 22:03:50 +0900

    add folder level test

---
+++ test.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+
+import chainer.cuda
+from chainer import serializers
+from chainercv.utils import read_image
+from chainercv.utils import write_image
+from chainercv.transforms import resize
+
+import net
+from config import root
+from dataset import Dataset
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', '-b', type=int, default=8)
+    parser.add_argument('--gpu', '-g', type=int, default=0,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--gen_class', '-c', default='Generator',
+                        help='Default generator class')
+    parser.add_argument("--load_gen_model", default='',
+                        help='load generator model')
+    parser.add_argument('--out', '-o', default='output',
+                        help='saved file name')
+    parser.add_argument("--resize_to", type=int, default=256,
+                        help='resize the image to')
+    parser.add_argument("--crop_to", type=int, default=256,
+                        help='crop the resized image to')
+    parser.add_argument("--load_dataset", default='horse2zebra/testA',
+                        help='load dataset')
+
+    args = parser.parse_args()
+    print(args)
+
+    if args.gpu >= 0:
+        chainer.cuda.get_device_from_id(args.gpu).use()
+
+    if not os.path.exists(args.out):
+        os.makedirs(args.out)
+
+    gen = getattr(net, args.gen_class)()
+
+    if args.load_gen_model != '':
+        serializers.load_npz(args.load_gen_f_model, gen)
+        print("Generator F model loaded")
+
+    if args.gpu >= 0:
+        gen.to_gpu()
+        print("use gpu {}".format(args.gpu))
+
+    dataset = Dataset(
+        path=os.path.join(root, args.load_dataset), resize_to=args.resize_to,
+        crop_to=args.crop_to, flip=False)
+
+    iterator = chainer.iterators.SerialIterator(dataset, args.batch_size,
+                                                repeat=False, shuffle=False)
+
+    xp = gen.xp
+    cnt = 0
+    for batch in iterator:
+        imgs = chainer.dataset.concat_examples(batch, device=args.gpu)
+        out = xp.asnumpy(gen(imgs).data)
+        for i in range(len(out)):
+            path = '{:s}/{:s}.jpg'.format(args.out, dataset.ids[cnt])
+            arr = (out[i] + 1.0) / 2.0 * 255.0
+            org = read_image(dataset.get_img_path(cnt))
+            _, h, w = org.shape
+            arr = resize(arr, (h, w))
+            write_image(arr, path)
+            cnt += 1
+            print(cnt)
